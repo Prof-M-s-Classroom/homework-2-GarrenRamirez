980c0c21fe439dd15048b15fdd5d2ad996843270
Garren Ramirez <gramirez4466@sdsu.edu>
2025-02-09 16:02:08 -0800

    Homework Assignment Completed

--- linkedlist.py
+++ linkedlist.py
@@ -67,7 +67,7 @@ class LinkedList:
             self.tail=None
         return temp
 
-    def print_list(self):   #Modified for Testing; Will Remove Upon Submission
+    def print_list(self):
        temp=self.head
        while temp is not None:
            print(temp.value)
@@ -76,7 +76,9 @@ class LinkedList:
     def insert_at_index(self, index, value):
         new_node = Node(value)
         if index == 0:
-            self.prepend(new_node)
+            return self.prepend(new_node)
+        elif index == self.length:
+            return self.append(new_node)
         elif index > self.length or index < 0:
             print("Invalid: Index Out of Bounds!")
             return False
@@ -96,16 +98,16 @@ class LinkedList:
         return True
 
     def delete_at_index(self, index):
-        if index < 0 or index > self.length:
-            print("Invalid: Index Out of Bounds")
-            return False
+        if index == 0:
+            return self.delfirst()
+        elif index == self.length - 1:
+            return self.dellast()
         elif self.length == 0:
             print("Invalid: List is Emtpy")
             return False
-        elif index == 0:
-            self.delfirst()
-        elif index == self.length:
-            self.dellast()
+        elif index < 0 or index >= self.length:
+            print("Invalid: Index Out of Bounds")
+            return False
         else:
             temp = self.head
             temp_index = 0
@@ -128,4 +130,69 @@ s4 = Spaceship("Challenger",400)
 s5 = Spaceship("Artemis",500)
 s6 = Spaceship("Pathfinder", 600)
 
-# Methods Implemented; Testing Tomorrow
+def test_linked_list():
+    ll = LinkedList(10)
+    ll.append(20)
+    ll.append(30)
+
+    print("Initial list:")
+    ll.print_list() #expecting 10, 20, 30
+
+    # Testing insert_at_index
+    print("\nInserting 15 at index 1:")
+    ll.insert_at_index(1, 15)
+    ll.print_list() #expecting 10, 15, 20, 30
+
+    print("\nInserting 5 at index 0:")
+    ll.insert_at_index(0, 5)
+    ll.print_list() #expecting 5, 10, 15, 20, 30
+
+    print("\nInserting 35 at index 5 (end of list):")
+    ll.insert_at_index(5, 35)
+    ll.print_list() #expecting 5, 10, 15, 20, 30, 35
+
+    print("\nTrying to insert at out-of-bounds index 10:")
+    ll.insert_at_index(10, 50)  #expecting Invalid
+
+    # Testing delete_at_index
+    print("\nDeleting element at index 2:")
+    ll.delete_at_index(2)
+    ll.print_list() #expecting 5, 10, 20, 30, 35
+
+    print("\nDeleting first element (index 0):")
+    ll.delete_at_index(0)
+    ll.print_list() #expecting 10, 20, 30, 35
+
+    print("\nDeleting last element (index 3):")
+    ll.delete_at_index(3)
+    ll.print_list() #expecting 10, 20, 30
+
+    print("\nTrying to delete at out-of-bounds index 10:")
+    ll.delete_at_index(10)  #expecting Invalid
+
+    print("\nTesting Node Linked List! ")
+    print("\n")
+    test = LinkedList(s2)
+    test.append(s3)
+    test.prepend(s1)
+    test.print_list()   #expecting s1, s2, s3
+    print("\n")
+    test.insert_at_index(0, s4)
+    test.print_list()   #expecting s4, s1, s2, s3
+    print("\n")
+    test.insert_at_index(5, s6) #expecting Invalid
+    test.insert_at_index(4, s6)
+    print("\n")
+    test.print_list()   #expecting s4, s1, s2, s3, s6
+    print("\n")
+    test.dellast()
+    test.delfirst()
+    test.print_list()   #expecting s1, s2, s3
+    print("\n")
+    test.delete_at_index(6) #expecting Invalid
+    test.delete_at_index(2)
+    print("\n")
+    test.print_list()   #expecting s1, s2
+
+    print("\nTesting Node Linked List Done!")
+test_linked_list()
